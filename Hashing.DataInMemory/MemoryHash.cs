24020a1d284a353f0e1011959abc007fc54d8ca0
antonio-dembo <beboantonio1@hotmail.com>
2022-05-02 22:15:16 -0300

    Adding Memory Hash example

---
+++ Hashing.DataInMemory/MemoryHash.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Hashing.DataInMemory
+{
+    public class MemoryHash
+    {
+        public byte[] HashDataInMemory(string massage)
+        {
+            var unicode = new UnicodeEncoding();
+            var byteMessage = unicode.GetBytes(massage);
+
+            var shaAlgorithm = new HMACSHA256();
+            return shaAlgorithm.ComputeHash(byteMessage);
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using Hashing.DataInMemory;
+using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string massage = "Hello, World";
+            byte[] digest = new MemoryHash().HashDataInMemory(massage);
+
+            foreach (byte b in digest)
+            {
+                Console.Write("{0:x}", b);
+            }
+
+            //Console.WriteLine(digest.Length);
+
+        }
+    }
+}
